ed31d33c4cf6bc16614f4dae70f60f0ca1d73338
Michele Ravaioli <michele.ravaioli3@studio.unibo.it>
2025-01-31 16:59:38 +0100

    update client.ts, user.ts for utility functions

--- frontend/src/utils/client.ts
+++ frontend/src/utils/client.ts
@@ -1,12 +1,43 @@
-import { CreateUserRequest } from "@gym-manager/models";
+import { Admin, CreateUserRequest, Role, Trainer, User } from "@gym-manager/models";
 
 export class Client {
+    private jwt?: string = undefined;
+
+    public get isLoggedIn(): boolean {
+        return this.jwt !== undefined;
+    }
 
     private apiRequest(method: string, endpoint: string, body?: object, headers?: Headers) {
         const h = headers || new Headers;
         h.append("Authorization", "Bearer " + "pizza");
         h.append("Content-Type", "application/json");
         return fetch(`/api/${endpoint}`, { method: method, body: JSON.stringify(body), headers: headers })
+    }
+
+    public async login(username: string, password: string): Promise<boolean> {
+        // TODO API request
+        this.jwt = "";
+        return true;
+    }
+
+    public get getUserDetails(): undefined | User | Trainer | Admin {
+        // TODO
+        const result: User = {
+            username: 'Rox09',
+            firstName: 'Rocco',
+            lastName: 'Siffredi',
+            id: '1',
+            dateOfBirth: '10/10/2020',
+            fiscalCode: 'RVLMLJC987DH43',
+            address: 'Via sghemba 4',
+            email: 'rsiffr@g.com',
+            phoneNumber: '+399333444555',
+        }
+        return result;
+    }
+    public get getRole(): undefined | Role {
+        // TODO
+        return Role.User;
     }
 
     public addUser(user: CreateUserRequest) {
