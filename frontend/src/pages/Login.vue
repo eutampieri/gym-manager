cb4534209f3e3e26e4b47e3c30f5eea4fad25f6c
Eugenio Tampieri <eugenio@eutampieri.eu>
2025-02-03 09:40:58 +0100

    Fix login spinner

--- frontend/src/pages/Login.vue
+++ frontend/src/pages/Login.vue
@@ -7,8 +7,11 @@ import { ref } from 'vue';
 const store = useUserStore();
 const username = ref<string>();
 const password = ref<string>();
+const loginInProgress = ref(false);
 async function login() {
+    loginInProgress.value = true;
     let authResult = await store.client.login(username.value || "", password.value || "");
+    loginInProgress.value = false;
     if (authResult) {
         router.push({ "path": "/admin" })
     }
@@ -17,19 +20,20 @@ async function login() {
 <template>
     <h1>Login</h1>
     <br>
-       <div class="form-group">
-       <label for="exampleInputEmail1">Email address</label>
-       <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-       </div>
-       <br>
-       <div class="form-group">
-       <label for="exampleInputPassword1">Password</label>
-       <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-       </div>
-       <br>
-       <!-- Bottone Login -->
-       <button class="btn btn-primary w-100" @click="login">
-        <span class="spinner-border spinner-border-sm me-2"></span>
+    <div class="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+            placeholder="Enter email">
+    </div>
+    <br>
+    <div class="form-group">
+        <label for="exampleInputPassword1">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    </div>
+    <br>
+    <!-- Bottone Login -->
+    <button class="btn btn-primary w-100" @click="login">
+        <span v-if="loginInProgress" class="spinner-border spinner-border-sm me-2"></span>
         Accedi
-       </button>
+    </button>
 </template>
