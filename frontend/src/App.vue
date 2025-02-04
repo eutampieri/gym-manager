3bc4ab75cb429fbeec2e10827367a82fefa26886
Michele Ravaioli <michele.ravaioli3@studio.unibo.it>
2025-02-04 19:43:20 +0100

    ops

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,7 +2,6 @@
 import Header from '@/components/Header.vue';
 import NotificationArea from '@/components/NotificationArea.vue';
 import { useNotificationsStore } from './store/notifications';
-import UserPage from './pages/UserPage.vue';
 
 const notifications = useNotificationsStore().notifications;
 </script>
@@ -10,6 +9,5 @@ const notifications = useNotificationsStore().notifications;
 <template>
     <Header></Header>
     <RouterView />
-    <UserPage/>
     <NotificationArea :notifications="notifications"></NotificationArea>
 </template>
