8749cb526a99d2f2497cd9db03529bc5cd4dc7d9
Eugenio Tampieri <eugenio@eutampieri.eu>
2025-02-04 18:08:03 +0100

    Notifications are removed from DOM after 10s

--- frontend/src/store/notifications.ts
+++ frontend/src/store/notifications.ts
@@ -2,11 +2,17 @@ import { defineStore } from 'pinia';
 import { ref } from 'vue';
 import { Notification as INotification } from '@/utils/notifications';
 
+const NOTIFICATION_DURATION = 10_000;
+
 export const useNotificationsStore = defineStore('notifications', () => {
     const notifications = ref<Array<INotification>>([]);
 
     return {
         notifications,
-        fire: (n: INotification) => notifications.value.push(n),
+        fire: (n: INotification) => {
+            const i = notifications.value.push(n) - 1;
+            console.log(i, notifications.value);
+            setTimeout(() => notifications.value.splice(0, 1), NOTIFICATION_DURATION);
+        },
     };
 });
