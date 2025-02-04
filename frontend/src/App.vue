43f8c50c5323c9a6132d09befef033bd3b6213f8
Michele Ravaioli <michele.ravaioli3@studio.unibo.it>
2025-02-04 19:42:08 +0100

    update user page and trainer page using session container

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,7 +2,7 @@
 import Header from '@/components/Header.vue';
 import NotificationArea from '@/components/NotificationArea.vue';
 import { useNotificationsStore } from './store/notifications';
-import BookCoursePage from './pages/BookCoursePage.vue';
+import UserPage from './pages/UserPage.vue';
 
 const notifications = useNotificationsStore().notifications;
 </script>
@@ -10,6 +10,6 @@ const notifications = useNotificationsStore().notifications;
 <template>
     <Header></Header>
     <RouterView />
-    <BookCoursePage/>
+    <UserPage/>
     <NotificationArea :notifications="notifications"></NotificationArea>
 </template>
