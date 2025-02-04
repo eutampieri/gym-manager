159049cded6915ba9768d4c2a8bd1e6f0a1e0034
Eugenio Tampieri <eugenio@eutampieri.eu>
2025-02-04 09:37:15 +0100

    Added message component

---
+++ frontend/src/components/Message.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps<{
+    message: string,
+    sentByCurrentUser: boolean,
+}>();
+</script>
+<template>
+    <p :class="'rounded p-2 text-bg-' + (sentByCurrentUser ? 'primary ms-auto' : 'tertiary')">{{ message }}</p>
+</template>
+<style lang="css" scoped>
+.text-bg-tertiary {
+    background-color: var(--bs-gray-400);
+}
+
+p {
+    max-width: min(calc(100% - 4em), 500px);
+    min-width: 200px;
+}
+</style>
