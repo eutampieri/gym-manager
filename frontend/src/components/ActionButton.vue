d5f6eab68f774fe6b84b6b37259f35b32f8f3bc9
Eugenio Tampieri <eugenio@eutampieri.eu>
2025-02-04 21:57:38 +0100

    Extract ActionButton

---
+++ frontend/src/components/ActionButton.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { Action } from '@/utils/lists';
+
+defineProps<{ action: Action, data: any }>();
+</script>
+<template>
+    <button type="button" :class="`btn btn-${action.colour} m-2`" @click="() => action.action(data)">{{ action.label
+        }}</button>
+</template>
