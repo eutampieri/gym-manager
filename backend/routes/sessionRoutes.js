01eb98f5fbc777e5d0037fa8588190c8acf0b93f
Eugenio Tampieri <eugenio@eutampieri.eu>
2025-02-05 17:24:14 +0100

    Wrapped all routes that needed wrapping

--- backend/routes/sessionRoutes.js
+++ backend/routes/sessionRoutes.js
@@ -1,23 +1,13 @@
 // Import modules
 const express = require('express');
-const { createAuthMiddleware } = require('../utils')
+const { wrapMiddleware, adminAuth, customerAuth, anyAuth } = require('../utils')
 const API = require('../controller/sessionApi');
 
-const customerRouter = express.Router();
-customerRouter.use(createAuthMiddleware(new Set(["admin", "customer"])));
-const customerAndTrainerRouter = express.Router();
-customerAndTrainerRouter.use(createAuthMiddleware(new Set(["admin", "customer", "trainer"])));
-const adminRouter = express.Router();
-adminRouter.use(createAuthMiddleware(new Set(["admin"])));
-
-customerRouter.post('/', API.createSession);
-adminRouter.get('/', API.fetchAllSessions);
-customerAndTrainerRouter.get("/:id", API.fetchSessionBy_Id)
-customerRouter.delete('/:id', API.deleteSession);
-
 const router = express.Router();
-router.use('/', adminRouter);
-router.use('/', customerRouter);
-router.use('/', customerAndTrainerRouter);
+
+router.post('/', wrapMiddleware(customerAuth, API.createSession));
+router.get('/', wrapMiddleware(adminAuth, API.fetchAllSessions));
+router.get("/:id", wrapMiddleware(anyAuth, API.fetchSessionBy_Id));
+router.delete('/:id', wrapMiddleware(customerAuth, API.deleteSession));
 
 module.exports = router;
--- backend/routes/trainerRoutes.js
+++ backend/routes/trainerRoutes.js
@@ -1,25 +1,15 @@
 const express = require('express')
-const { createAuthMiddleware } = require('../utils')
+const { wrapMiddleware, adminAuth, trainerAuth, customerAuth, anyAuth } = require('../utils')
 const API = require('../controller/trainerApi')
 
-const customerRouter = express.Router();
-customerRouter.use(createAuthMiddleware(new Set(["admin", "customer"])));
-const adminRouter = express.Router();
-adminRouter.use(createAuthMiddleware(new Set(["admin"])));
-const trainerRouter = express.Router();
-trainerRouter.use(createAuthMiddleware(new Set(["admin", "trainer"])));
-
-adminRouter.post("/", API.createTrainer)
-adminRouter.get("/", API.fetchAllTrainers)
-customerRouter.get("/:id", API.fetchTrainerBy_Id)
-adminRouter.put("/", API.updateTrainer)
-adminRouter.delete("/:id", API.deleteTrainer)
-trainerRouter.get("/:id/courses", API.fetchAllTrainerCourses)
-trainerRouter.get("/:id/sessions", API.fetchAllTrainerSessions)
-
 const router = express.Router();
-router.use('/', adminRouter);
-router.use('/', trainerRouter);
-router.use('/', customerRouter);
+
+router.post("/", wrapMiddleware(adminAuth, API.createTrainer));
+router.get("/", wrapMiddleware(adminAuth, API.fetchAllTrainers));
+router.get("/:id", wrapMiddleware(customerAuth, API.fetchTrainerBy_Id));
+router.put("/", wrapMiddleware(adminAuth, API.updateTrainer));
+router.delete("/:id", wrapMiddleware(adminAuth, API.deleteTrainer));
+router.get("/:id/courses", wrapMiddleware(trainerAuth, API.fetchAllTrainerCourses));
+router.get("/:id/sessions", wrapMiddleware(trainerAuth, API.fetchAllTrainerSessions));
 
 module.exports = router
