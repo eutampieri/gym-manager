40fe9e056625f342c76bd75b110668deab433b2d
Michele Ravaioli <michele.ravaioli3@studio.unibo.it>
2025-02-08 00:03:39 +0100

    responsive table in desktop list view

--- frontend/src/components/DesktopListView.vue
+++ frontend/src/components/DesktopListView.vue
@@ -6,17 +6,19 @@ defineProps<{ data: ListData }>();
 
 </script>
 <template>
-    <table class="table table-striped align-middle">
-        <thead>
-            <tr>
-                <th v-for="h in data.headers" scope="col" :id="h.key">{{ h.name }}</th>
-                <th scope="col" id="actions" aria-label="Actions"></th>
-            </tr>
-        </thead>
-        <tbody>
-            <DesktopListRow v-for="(row, index) in data.data" :key='index' :actions="data.actions" :data="row"
-                :headers="data.headers">
-            </DesktopListRow>
-        </tbody>
-    </table>
+    <div class="table-responsive">
+        <table class="table table-striped align-middle">
+            <thead>
+                <tr>
+                    <th v-for="h in data.headers" scope="col" :id="h.key">{{ h.name }}</th>
+                    <th scope="col" id="actions" aria-label="Actions"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <DesktopListRow v-for="(row, index) in data.data" :key='index' :actions="data.actions" :data="row"
+                    :headers="data.headers">
+                </DesktopListRow>
+            </tbody>
+        </table> 
+    </div>
 </template>
