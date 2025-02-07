756ddbb9dc44e89b3185de1dc42f1b47d760c044
Eugenio Tampieri <eugenio@eutampieri.eu>
2025-02-07 18:48:43 +0100

    Better tables on desktop, better list cells on mobile

--- frontend/src/components/MobileListView.vue
+++ frontend/src/components/MobileListView.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ListData, RowData } from '@/utils/lists';
-import TableRow from './TableRow.vue';
+import MobileListCell from './MobileListCell.vue';
 import Dropdown from './Dropdown.vue';
 import DropdownItem from './DropdownItem.vue';
 
@@ -8,9 +8,9 @@ defineProps<{ data: ListData, accordionHeader: (d: RowData) => string }>();
 </script>
 <template>
     <Dropdown id="mobile-list">
-        <DropdownItem v-for="(row, index) in data.data" :key='index' :header="[accordionHeader(row)]" :dropdown-id="'mobile-list'" :id-prefix="'mlist-item'" :index="index">
-            <TableRow :actions="data.actions" :data="row" :headers="data.headers" :show-headers="true"
-                        class="col-sm-12"></TableRow>
+        <DropdownItem v-for="(row, index) in data.data" :key='index' :header="[accordionHeader(row)]"
+            :dropdown-id="'mobile-list'" :id-prefix="'mlist-item'" :index="index">
+            <MobileListCell :actions="data.actions" :data="row" :headers="data.headers"></MobileListCell>
         </DropdownItem>
     </Dropdown>
 </template>
