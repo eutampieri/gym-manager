b675b304ab6cec3d07308a16d815d2316016ed44
Eugenio Tampieri <eugenio@eutampieri.eu>
2025-02-07 17:03:51 +0100

    Better client path

--- frontend/src/utils/client.ts
+++ frontend/src/utils/client.ts
@@ -14,6 +14,15 @@ export class Client {
     private impersonating: boolean = false;
     private impersonatedInfo: { role: Role, user: User | Trainer | undefined } = { role: Role.Admin, user: undefined };
 
+    private apiRequest(method: string, endpoint: string, body?: object, headers?: Headers) {
+        const h = headers || new Headers;
+        if (this.jwt !== undefined) {
+            h.append("Authorization", "Bearer " + this.jwt);
+        }
+        h.append("Content-Type", "application/json");
+        return fetch(`/api${endpoint}`, { method: method, body: JSON.stringify(body), headers: h })
+    }
+
     public get isLoggedIn(): boolean {
         // check if token is defined and valid
         try {
@@ -42,15 +51,6 @@ export class Client {
     public stopImpersonating() {
         this.impersonating = false;
         this.impersonatedInfo.user = undefined;
-    }
-
-    private apiRequest(method: string, endpoint: string, body?: object, headers?: Headers) {
-        const h = headers || new Headers;
-        if (this.jwt !== undefined) {
-            h.append("Authorization", "Bearer " + this.jwt);
-        }
-        h.append("Content-Type", "application/json");
-        return fetch(`/api${endpoint}`, { method: method, body: JSON.stringify(body), headers: h })
     }
 
     public async login(username: string, password: string): Promise<boolean> {
